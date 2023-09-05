5e6c2dbce4c312c2902ab421b1f880436fd616cc
David Horner <dave@thehorners.com>
2023-09-05 02:20:32 -0400

    done playing for the night

--- examples/add_scene.rs
+++ examples/add_scene.rs
@@ -1,7 +1,8 @@
 use std::{env, time::Duration};
+use serde_json::json;
 
 use anyhow::Result;
-use obws::{Client, requests::{inputs}};
+use obws::{Client, requests::{inputs::{self, SetSettings}, scene_items::{SetTransform, SceneItemTransform, Scale, Id, SetPrivateSettings}}, common::Alignment};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -49,11 +50,23 @@ println!("{:#?}", client.inputs().default_settings::<serde_json::Value>("vlc_sou
             .set_current_program_scene(&scene_name)
             .await.ok();
         //client.inputs().remove(&source).await?;
-let dd=client.inputs().settings::<serde_json::Value>(source).await.ok();
-let dd=dbg!(dd);
-client.inputs().remove(source).await.ok();
-        println!("dude: {:?}",&dd);
-client
+let mut dd=client.inputs().settings::<serde_json::Value>(&source).await.ok();
+let source_id=client.scene_items().id(Id{scene:&scene_name ,source:&source,search_offset:None}).await;
+match source_id {
+    Ok(id)=>{
+        println!("SOURCE ID: {}",id);
+        let mut p=client.scene_items().private_settings::<serde_json::Value>(&scene_name, id).await?;
+        println!("SOURCE SETTINGS: {} {}",p,"test");
+        if input!=&p["input"].to_string() {
+        p["input"]=json!(input);
+        client.scene_items().set_private_settings(SetPrivateSettings{scene:&scene_name, item_id: id,settings:&p}).await;
+        }else {
+        println!("SOURCE ALREADY: {}",input);
+        }
+    },
+    Err(e)=>{
+        println!("SOURCE NONE {}",e);
+let item_id=client
     .inputs()
     .create(inputs::Create {
         scene: &scene_name,
@@ -68,5 +81,46 @@ client
         enabled: None,
     })
     .await?;
+    // scene:&scene_name,
+    // item_id: item_id,
+    let mut t: SceneItemTransform = Default::default();
+    t.scale=Some(Scale{x:Some(1.0),y:Some(1.0)});
+client.scene_items().set_transform(
+     SetTransform {item_id: item_id,scene: &scene_name, transform:t
+    //     position: Some(Position {
+    //         x: Some(0),
+    //         y: Some(0),
+    //     }),
+    //     rotation: Some(0),
+    //     scale: Some(Scale {
+    //         x: Some(1),
+    //         y: Some(1),
+    //     }),
+    //     alignment: Some(t.alignment),
+    //     bounds: Some(Bounds {
+    //         r#type: Some(t.bounds_type),
+    //         alignment: Some(t.bounds_alignment),
+    //         width: Some(t.bounds_width),
+    //         height: Some(t.bounds_height),
+    //     }),
+    //     crop: Some(Crop {
+    //         left: Some(t.crop_left),
+    //         right: Some(t.crop_right),
+    //         top: Some(t.crop_top),
+    //         bottom: Some(t.crop_bottom),
+    //     }),
+    // }
+     }).await?;
+    },
+}
+let mut dd=dbg!(dd);
+match dd {
+//Some(ref mut x)=>x.settings["input"] = json!(input),
+Some(ref mut x)=>x.settings["input"] = json!("dave"),
+None=>{}
+}
+println!("dude: {:?}",&dd);
+let s=client.inputs().set_settings(SetSettings{input:input,settings:&dd,overlay:Some(false)}).await;
+//client.inputs().remove(source).await.ok();
     Ok(())
 }
